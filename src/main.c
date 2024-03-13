1e65eba59c6b87fb249060d70ee11c663452a686
pokenem <73646157+pokenem@users.noreply.github.com>
2024-03-13 13:48:16 +0000

    add functions of swap nums in array and compare of sums of digits.

--- src/main.c
+++ src/main.c
@@ -11,6 +11,20 @@ int sumOfDigits(int num) {
     return sum;
 }
 
+// Функция для сравнения двух чисел по сумме цифр
+int compare(int num1, int num2) {
+    int sum1 = sumOfDigits(num1);
+    int sum2 = sumOfDigits(num2);
+    return sum1 - sum2;
+}
+
+// Функция для обмена двух элементов массива
+void swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
 int main() {
     int n;
     printf("Введите число: ");
