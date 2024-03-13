2cdd1d7a049e9036fdcb262ebaaf99bed2bba24e
pokenem <73646157+pokenem@users.noreply.github.com>
2024-03-13 13:50:55 +0000

    add sort function

--- .ccls-cache/@home@runner@lab3-task1-gr11-stepanov/src@main.c
+++ .ccls-cache/@home@runner@lab3-task1-gr11-stepanov/src@main.c
@@ -1,4 +1,3 @@
-/* hello.c */
 #include <stdio.h>
 
 // Функция для подсчета суммы цифр числа
@@ -25,11 +24,35 @@ void swap(int* a, int* b) {
     *b = temp;
 }
 
+// Функция для сортировки массива чисел по сумме цифр
+void sortByDigitSum(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (compare(arr[j], arr[j + 1]) > 0) {
+                swap(&arr[j], &arr[j + 1]);
+            }
+        }
+    }
+}
+
 int main() {
     int n;
-    printf("Введите число: ");
+    printf("Введите размерность массива: ");
     scanf("%d", &n);
-    printf("Сумма цифр: ");
-    printf("%d", sumOfDigits(n));
+
+    int arr[n];
+    printf("Введите элементы массива:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    sortByDigitSum(arr, n);
+
+    printf("Отсортированный массив:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
     return 0;
 }
