dbd7f0b95a3cb4c38093e4cd26a03cf257a538da
pokenem <73646157+pokenem@users.noreply.github.com>
2024-03-13 13:47:22 +0000

    add function of sum of digits in num

--- src/main.c
+++ src/main.c
@@ -1,8 +1,21 @@
 /* hello.c */
 #include <stdio.h>
 
-void main (void)
-{
-	printf ("Hello World\n");
-	printf ("Goodbye World\n");
+// Функция для подсчета суммы цифр числа
+int sumOfDigits(int num) {
+    int sum = 0;
+    while (num != 0) {
+        sum += num % 10;
+        num /= 10;
+    }
+    return sum;
+}
+
+int main() {
+    int n;
+    printf("Введите число: ");
+    scanf("%d", &n);
+    printf("Сумма цифр: ");
+    printf("%d", sumOfDigits(n));
+    return 0;
 }
